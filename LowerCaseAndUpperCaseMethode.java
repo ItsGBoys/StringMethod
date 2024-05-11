00fe9ae6aa0d330b477aad3672491f27288f1640
Maulana Ibrahim <120733899+ItsGBoys@users.noreply.github.com>
2024-05-11 19:16:33 +0700

    Create LowerCaseAndUpperCaseMethode.java

---
+++ LowerCaseAndUpperCaseMethode.java
@@ -0,0 +1,5 @@
+String str = "Hello, World!";
+String lowerCase = str.toLowerCase();
+String upperCase = str.toUpperCase();
+System.out.println("Lowercase: " + lowerCase); // Output: Lowercase: hello, world!
+System.out.println("Uppercase: " + upperCase); // Output: Uppercase: HELLO, WORLD!
