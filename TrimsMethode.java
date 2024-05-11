99029c1a2538de55938544a74217a27dc13b67de
Maulana Ibrahim <120733899+ItsGBoys@users.noreply.github.com>
2024-05-11 19:14:53 +0700

    Create TrimsMethode.java

---
+++ TrimsMethode.java
@@ -0,0 +1,3 @@
+String str = "   Hello, World!   ";
+String trimmedStr = str.trim();
+System.out.println("String setelah di-trim: '" + trimmedStr + "'"); // Output: String setelah di-trim: 'Hello, World!'
