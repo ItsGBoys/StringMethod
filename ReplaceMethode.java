080ba29259ec5d5571338a56a4c47e5713793318
Maulana Ibrahim <120733899+ItsGBoys@users.noreply.github.com>
2024-05-11 19:17:04 +0700

    Create ReplaceMethode.java

---
+++ ReplaceMethode.java
@@ -0,0 +1,3 @@
+String str = "Hello, World!";
+String replacedStr = str.replace("Hello", "Hi");
+System.out.println("String setelah penggantian: " + replacedStr); // Output: String setelah penggantian: Hi, World!
